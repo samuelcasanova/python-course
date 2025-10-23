3ae58d204453857f12d01bf98bfd8b7aeeb44955
samuel.casanova <samuel.casanova@immfly.com>
2025-10-23 10:36:34 +0200

    add loops and more conditions to the if statements

--- src/code_blocks.py
+++ src/code_blocks.py
@@ -1,10 +1,44 @@
-print("If statement:")
+print("\nIf statement:")
 temperature = 23
-if temperature > 25:
+thermometer_working = True
+if temperature > 25 and thermometer_working:
     print("It's a hot day.")
-elif temperature < 15:
+elif temperature < 15 and thermometer_working:
     print("It's a cold day.")
 else:
     print("It's a pleasant day.")
 print("Enjoy your day!")
 
+print("\nTernary operator:")
+temperature = 23
+have_beer = False
+print("It's a hot day." if temperature > 25 or not have_beer else "It's a cold day." if temperature < 15 else "It's a pleasant day.")
+
+print("\nChaining comparisons:")
+temperature = 23
+if 20 < temperature < 30:
+    print("The weather is nice.")
+
+print("\nFor loop with an else statement:")
+successful = False
+for i in range(2, 10, 2): # range is an iterable
+    print(f"Attempt: {i} {i * "."}")
+    if successful:
+        print("Success!")
+        break
+else:
+    print("Attempted all options without success.")
+
+print("\nFor loop using a string, which is an iterable:")
+for char in "Python":
+    print(char)
+
+print("\nFor loop using an iterable list:")
+for language in ["Python", "Javascript", "Ruby"]:
+    print(language)
+
+print("\nWhile loop:")
+number = 20
+while number > 10:
+    print(number)
+    number -= 5
