c458b4725fa37bd9402d5d9d208c00743265f74f
samuel.casanova <samuel.casanova@immfly.com>
2025-10-27 08:48:56 +0100

    fix run-basics.sh

--- run-basic.sh
+++ run-basic.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 echo -e "\n***********\nRunning Python basic prints..."
-python3 ./src/basics.py
+python3 ./src/basics/basics.py
 
 # echo -e "\n***********\nHandling user input..."
-# python3 ./src/input.py
+# python3 ./src/basics/input.py
 
 echo -e "\n***********\nUsing types..."
-python3 ./src/types.py
+python3 ./src/basics/types.py
 
 echo -e "\n***********\nWorking with strings..."
-python3 ./src/strings.py
+python3 ./src/basics/strings.py
 
 echo -e "\n***********\nPerforming math operations..."
-python3 ./src/math.py
+python3 ./src/basics/math.py
 
 echo -e "\n***********\nUsing code blocks..."
-python3 ./src/code_blocks.py
+python3 ./src/basics/code_blocks.py
 
 echo -e "\n***********\nDefining and calling functions..."
-python3 ./src/functions.py
+python3 ./src/basics/functions.py
