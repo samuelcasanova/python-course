272f8888a79707c2ff486beb254a7362fd9a6edb
samuel.casanova <samuel.casanova@immfly.com>
2025-10-27 08:36:44 +0100

    add sets

--- run-advanced.sh
+++ run-advanced.sh
@@ -4,3 +4,9 @@ python3 ./src/advanced/lists.py
 
 echo -e "\n***********\nWorking with tuples..."
 python3 ./src/advanced/tuples.py
+
+echo -e "\n***********\nWorking with dictionaries..."
+python3 ./src/advanced/dictionaries.py
+
+echo -e "\n***********\nWorking with sets..."
+python3 ./src/advanced/sets.py
